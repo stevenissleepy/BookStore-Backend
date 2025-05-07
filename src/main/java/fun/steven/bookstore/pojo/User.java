c4fbd702a893d29a881699200d4ac66994322ac0
steven <1085320540@qq.com>
2025-05-07 17:06:15 +0800

    feat: 添加 user 类

---
+++ src/main/java/fun/steven/bookstore/pojo/User.java
@@ -0,0 +1,46 @@
+package fun.steven.bookstore.pojo;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Table(name = "user")
+@Entity
+public class User {
+    @Id
+    @Column(name = "user_id")
+    private String userId;
+    @Column(name = "user_name")
+    private String userName;
+    @Column(name = "password")
+    private String password;
+
+    public String getUserId() {
+        return userId;
+    }
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+    public String getUserName() {
+        return userName;
+    }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId='" + userId + '\'' +
+                ", userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
