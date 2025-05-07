eae9e130da6b749cf8caf85c10872039853b0847
steven <1085320540@qq.com>
2025-05-07 17:18:26 +0800

    feat: 完成了 Response Message 设计

---
+++ src/main/java/fun/steven/bookstore/pojo/ResponseMessage.java
@@ -0,0 +1,39 @@
+package fun.steven.bookstore.pojo;
+
+public class ResponseMessage<T> {
+    private Integer code;
+    private String message;
+    private T data;
+
+    /* get and set */
+    public Integer getCode() {
+        return code;
+    }
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+    public String getMessage() {
+        return message;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    public T getData() {
+        return data;
+    }
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    /* constructor */
+    public ResponseMessage(Integer code, String message, T data) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+
+    /* method */
+    public static <T> ResponseMessage<T> success(T data) {
+        return new ResponseMessage<>(200, "success", data);
+    }
+}
