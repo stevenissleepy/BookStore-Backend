46a982520cd273af99cc036c37add8a6125d29a3
steven <1085320540@qq.com>
2025-05-11 11:52:41 +0800

    feat: add book entity

---
+++ src/main/java/fun/steven/bookstore/entity/Book.java
@@ -0,0 +1,30 @@
+package fun.steven.bookstore.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_book")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "book_id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "price")
+    private Double price;
+
+    @Column(name = "cover")
+    private String cover;
+}
