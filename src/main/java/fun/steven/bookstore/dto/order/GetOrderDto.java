41d36c00c159b10f2a26c550011b2911a3400db5
steven <1085320540@qq.com>
2025-05-13 09:39:15 +0800

    feat: 完善了 order dto

--- src/main/java/fun/steven/bookstore/dto/order/GetOrderDto.java
+++ src/main/java/fun/steven/bookstore/dto/order/GetOrderDto.java
@@ -1,5 +1,12 @@
 package fun.steven.bookstore.dto.order;
 
+import java.util.List;
+
+import fun.steven.bookstore.dto.address.AddressDto;
+import lombok.Data;
+
+@Data
 public class GetOrderDto {
-    
+    private AddressDto address;
+    private List<GetOrderItemDto> orderItems;
 }
--- src/main/java/fun/steven/bookstore/dto/order/GetOrderItemDto.java
+++ src/main/java/fun/steven/bookstore/dto/order/GetOrderItemDto.java
@@ -1,5 +1,10 @@
 package fun.steven.bookstore.dto.order;
 
+import fun.steven.bookstore.dto.book.BookDto;
+import lombok.Data;
+
+@Data
 public class GetOrderItemDto {
-    
+    private BookDto book;
+    private Integer quantity;
 }
