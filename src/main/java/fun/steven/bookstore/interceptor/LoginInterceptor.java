33a198c81b4001721f6d34d7bcfea42771cd6e14
steven <1085320540@qq.com>
2025-05-13 08:35:11 +0800

    fix: interceptor

--- src/main/java/fun/steven/bookstore/interceptor/LoginInterceptor.java
+++ src/main/java/fun/steven/bookstore/interceptor/LoginInterceptor.java
@@ -18,8 +18,9 @@ public class LoginInterceptor implements HandlerInterceptor {
         @NonNull Object handler
     ) throws Exception {
 
-        Long user = (Long) request.getSession().getAttribute("userId");
-        if (user == null) {
+        // 从 Session 中获取用户 ID
+        Long userId = (Long) request.getSession().getAttribute("userId");
+        if (userId == null) {
             throw new LoginException("未登录或登录超时，请重新登录");
         }
         return true;
