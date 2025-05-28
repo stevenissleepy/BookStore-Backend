871a4f01c10d3f61da332182250cf6cfc8f70bf1
steven <1085320540@qq.com>
2025-05-28 16:21:03 +0800

    Revert "feat: cartItem id 会返回给前端"
    This reverts commit e0d418b09c5a48373484bb52b8b1eb97b29cbbac.

--- src/main/java/fun/steven/bookstore/dto/cart/GetCartItemDto.java
+++ src/main/java/fun/steven/bookstore/dto/cart/GetCartItemDto.java
@@ -10,12 +10,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class GetCartItemDto {
-    private Long id;
     private BookDto book;
     private Integer quantity;
 
     public GetCartItemDto(CartItem cartItem) {
-        this.id = cartItem.getId();
         this.book = new BookDto(cartItem.getBook());
         this.quantity = cartItem.getQuantity();
     }
